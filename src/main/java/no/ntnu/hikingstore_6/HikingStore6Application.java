69ca9623153b35f39cb451714638f543d75e7ae4
Jonas <jonaskh@stud.ntnu.no>
2022-02-09 11:54:13 +0100

    initial commit

---
+++ src/main/java/no/ntnu/hikingstore_6/HikingStore6Application.java
@@ -0,0 +1,13 @@
+package no.ntnu.hikingstore_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingStore6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikingStore6Application.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/hikingstore_6/HikingStore6ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.hikingstore_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingStore6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

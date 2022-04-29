0b6ebb05aacd8dd3235af93a92395fd979f423d0
Jonas Klinge Husøy <jonaskh@stud.ntnu.no>
2022-04-29 12:29:09 +0200

    More frontpage edits

--- src/main/java/no/ntnu/hikingstore_6/service/ProductService.java
+++ src/main/java/no/ntnu/hikingstore_6/service/ProductService.java
@@ -2,8 +2,8 @@ package no.ntnu.hikingstore_6.service;
 
 import no.ntnu.hikingstore_6.models.Product;
 import no.ntnu.hikingstore_6.repositories.ProductRepository;
-import no.ntnu.models.Product;
-import no.ntnu.repositories.ProductRepository;
+import no.ntnu.hikingstore_6.models.Product;
+import no.ntnu.hikingstore_6.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 

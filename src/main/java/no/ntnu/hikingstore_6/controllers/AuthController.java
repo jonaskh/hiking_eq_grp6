a5ec74616b37430ca5e9243452b4103489567fce
Jonaskh <jonaskh@stud.ntnu.no>
2022-06-05 15:01:16 +0200

    idk

--- src/main/java/no/ntnu/hikingstore_6/controllers/AuthController.java
+++ src/main/java/no/ntnu/hikingstore_6/controllers/AuthController.java
@@ -1,15 +1,17 @@
 package no.ntnu.hikingstore_6.controllers;
 
+import no.ntnu.hikingstore_6.entities.User;
 import no.ntnu.hikingstore_6.models.AuthenticationRequest;
 import no.ntnu.hikingstore_6.models.AuthenticationResponse;
+import no.ntnu.hikingstore_6.repositories.UserRepository;
 import no.ntnu.hikingstore_6.security.JwtUtil;
-import no.ntnu.hikingstore_6.service.AccessUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.validation.annotation.Validated;
@@ -19,35 +21,33 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AuthController {
 
+    @Autowired
+    AuthenticationManager authManager;
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserRepository userRepository;
 
     @Autowired
-    private AuthenticationManager authenticationManager;
+    JwtUtil jwtUtil;
 
-    @Autowired
-    private JwtUtil jwtTokenUtil;
-
-    @Autowired
-    private AccessUserService accessUserService;
-
-
-    @RequestMapping(value = "/api/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> login(@RequestBody @Validated AuthenticationRequest request) {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                    authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+            Authentication authentication = authManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getUsername(), request.getPassword())
+                    );
+            User user = (User) authentication.getPrincipal();
+            String accessToken = jwtUtil.generateAccessToken(user);
+            AuthenticationResponse response = new AuthenticationResponse(accessToken);
+
+            return ResponseEntity.ok().body(response);
         } catch (BadCredentialsException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-
-        final UserDetails userDetails = accessUserService.
-                loadUserByUsername(authenticationRequest.getUsername());
-        final String jwt = jwtTokenUtil.generateToken(userDetails);
-
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
+
+
 
     @RequestMapping("/hello")
     public String hello() {
--- src/main/java/no/ntnu/hikingstore_6/security/JwtUtil.java
+++ src/main/java/no/ntnu/hikingstore_6/security/JwtUtil.java
@@ -1,60 +1,60 @@
 package no.ntnu.hikingstore_6.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import no.ntnu.hikingstore_6.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
 
 @Component
 public class JwtUtil {
+    private static final long EXPIRE_DURATION = 24 * 60 * 60 * 1000; // 24 hour
 
     @Value("${jwt_secret_key}")
-    private String secret_key;
+    private String SECRET_KEY;
 
-    private static final String JWT_AUTH_KEY = "roles";
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtil.class);
 
-    public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        return Jwts.builder().setSubject(userDetails.getUsername()).claim(JWT_AUTH_KEY, userDetails.getAuthorities())
-                .setIssuedAt(new Date(System.currentTimeMillis())).setExpiration(new Date(System.currentTimeMillis()
-                        + 1000 * 60 * 60 * 10)).signWith(SignatureAlgorithm.HS256,secret_key).compact();
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .setSubject(String.format("%s,%s", user.getUsername(), user.getPassword()))
+                .setIssuer("CodeJava")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .compact();
     }
 
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
+    public boolean validateAccessToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException ex) {
+            LOGGER.error("JWT expired", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            LOGGER.error("Token is null, empty or only whitespace", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            LOGGER.error("JWT is invalid", ex);
+        } catch (UnsupportedJwtException ex) {
+            LOGGER.error("JWT is not supported", ex);
+        } catch (SignatureException ex) {
+            LOGGER.error("Signature validation failed");
+        }
+
+        return false;
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
+    public String getSubject(String token) {
+        return parseClaims(token).getSubject();
     }
 
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret_key).parseClaimsJws(token).getBody();
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
     }
-
-    private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-
-
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
-
 }
-

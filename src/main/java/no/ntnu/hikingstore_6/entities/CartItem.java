14f068c7e587b44e1c5650780d1b459a6fb26faf
Stian B Kaspersen <stianbka@ntnu.stud.no>
2022-06-06 14:07:06 +0100

    Added Cart class

---
+++ src/main/java/no/ntnu/hikingstore_6/entities/CartItem.java
@@ -0,0 +1,68 @@
+package no.ntnu.hikingstore_6.entities;
+
+
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private int quantity;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "id=" + id +
+                ", product=" + product +
+                ", user=" + user +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
